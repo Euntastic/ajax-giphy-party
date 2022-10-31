8ee7b501952d17212239ddf2b5b3d6d84e1ce197
Eun Lee <eun@euntastic.dev>
2022-10-31 16:18:47 -0400

    Commit

--- app.js
+++ app.js
@@ -2,21 +2,6 @@
 const APIKEY = 'XE4xhQrlMTcvFXims9MeGB0zze91kRAC'
 let columnCounter = 1;
 
-async function searchGiphy(query = 'minions') {
-  let temp_url = 'http://api.giphy.com/v1/gifs/search';
-
-  const response = await axios.get(temp_url, {
-    params: {
-      q: query,
-      api_key: APIKEY,
-      limit: 10
-    }
-  });
-
-  let randomNumber = Math.floor(Math.random() * 10);
-  return String(response.data.data[randomNumber].url);
-}
-
 function randomTestImg() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber == 1) return "https://media.giphy.com/media/VRhsYYBw8AE36/giphy.gif";
@@ -24,12 +9,22 @@ function randomTestImg() {
   if (randomNumber == 3) return 'https://media.giphy.com/media/l3HBbltOYjoNq/giphy.gif';
 }
 
-function createImg(event) {
+async function createImg(event) {
   event.preventDefault();
+  let baseUrl = 'http://api.giphy.com/v1/gifs/search';
 
-  // testURL = searchGiphy($('#search-query').value);
-  let testURL = randomTestImg();
-  const newImg = $('<img>').attr('src', testURL);
+  const response = await axios.get(baseUrl, {
+    params: {
+      q: $('#search-query').val(),
+      api_key: APIKEY,
+      limit: 10
+    }
+  });
+
+  let randomNumber = Math.floor(Math.random() * 10);
+  let tempString = response.data.data[randomNumber].images.original.url;
+
+  const newImg = $('<img>').attr('src', tempString);
   newImg.addClass('rounded-xl mb-2 w-full');
 
   $(`#giphy-col-${columnCounter}`).append(newImg);
